3f278c09b4cdbf45d608cf2741102b062e383aad
Yash-Netankar <netankaryash2002@gmail.com>
2020-09-28 16:19:11 +0530

    added navbar, added animations

--- js.js
+++ js.js
@@ -1,7 +1,14 @@
 const slides = document.querySelector(".slider").children;
 const prev_btn = document.querySelector(".pre");
 const next_btn = document.querySelector(".next");
+const submenu_btn = document.querySelector(".submenu-btn");
+const submenu = document.querySelector(".submenu");
+const burger = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const close_btn = document.querySelector(".close-btn");
 
+
+// slides
 let index = 0;
 
 prev_btn.addEventListener("click", ()=>{
@@ -39,4 +46,41 @@ function changeSlide(){
     slides[index].classList.add("active");
 }
 
+
+
+// Submenu
+submenu_btn.addEventListener("click", ()=>{
+    submenu.classList.toggle("showSubmenu");
+
+    let anywhere = document.querySelectorAll(".slide");
+    let i = 0;
+    while(i < anywhere.length)
+    {
+        anywhere[i].addEventListener("click", ()=>{
+            submenu.classList.remove("showSubmenu");
+        });
+        i++;
+    }
+});
+
+
+// sidebar
+burger.addEventListener("click",() =>{
+    sidebar.classList.toggle("show-sidebar");
+
+    let anywhere = document.querySelectorAll(".slide");
+    let i = 0;
+    while(i < anywhere.length)
+    {
+        anywhere[i].addEventListener("click", ()=>{
+            sidebar.classList.remove("show-sidebar");
+        });
+        i++;
+    }
+
+});
+
+close_btn.addEventListener("click", ()=>{
+    sidebar.classList.remove("show-sidebar");
+});
 
