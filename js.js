ff615e33621a01e970233f0f91df5090d1617a61
Yash-Netankar <netankaryash2002@gmail.com>
2020-09-29 20:55:06 +0530

    added working search-bar and styled the play now button

--- js.js
+++ js.js
@@ -84,3 +84,31 @@ close_btn.addEventListener("click", ()=>{
     sidebar.classList.remove("show-sidebar");
 });
 
+// search bar script
+function myFunction() {
+      let input, filter, ul, li, a, i, txtValue;
+      input = document.querySelector('.search-txt');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName('li');
+    
+      for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    }
+
+
+
+
+
+
+
+
+
+
